1a002b7ea2d394637e450da43403f76b47ff25a3
GitHub Action <action@github.com>
2022-02-02 12:36:38 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/divyasaravanan/customplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.divyasaravanan.customplugin.services
+
+import com.github.divyasaravanan.customplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
